c6b6930198a5fcc54aa9987dd29ef0fb46efaa42
satty-dev <sadahiro0814@gmail.com>
2025-04-10 00:54:17 +0900

    TOPページに各ページの導線を仮実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+import Link from 'next/link';
+
 // components
 import { Template } from '@/components/layouts/Template';
 
 export default function Home() {
-    return <Template>Hello World!</Template>;
+    return (
+        <Template>
+            Hello World!
+            <ul>
+                <li>
+                    <Link href={{ pathname: '/works' }}>works</Link>
+                </li>
+                <li>
+                    {' '}
+                    <Link href={{ pathname: '/blog' }}>blog</Link>
+                </li>
+            </ul>
+        </Template>
+    );
 }
