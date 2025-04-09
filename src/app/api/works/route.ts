4c8ee0926230b0d2f98e80edad2e289fc79159a5
satty-dev <sadahiro0814@gmail.com>
2025-04-10 01:05:29 +0900

    【works】API型修正

--- src/app/api/works/route.ts
+++ src/app/api/works/route.ts
@@ -9,30 +9,35 @@ const works: TWork[] = [
         title: 'タイトル1',
         description: '実績の説明を簡潔にします。',
         image: 'https://static.vecteezy.com/vite/assets/photo-masthead-375-BoK_p8LG.webp',
+        link: '',
     },
     {
         id: '2',
         title: 'タイトル2',
         description: '実績の説明を簡潔にします。',
         image: 'https://static.vecteezy.com/vite/assets/photo-masthead-375-BoK_p8LG.webp',
+        link: '',
     },
     {
         id: '3',
         title: 'タイトル3',
         description: '実績の説明を簡潔にします。',
         image: 'https://static.vecteezy.com/vite/assets/photo-masthead-375-BoK_p8LG.webp',
+        link: '',
     },
     {
         id: '4',
         title: 'タイトル4',
         description: '実績の説明を簡潔にします。',
         image: 'https://static.vecteezy.com/vite/assets/photo-masthead-375-BoK_p8LG.webp',
+        link: '',
     },
     {
         id: '5',
         title: 'タイトル5',
         description: '実績の説明を簡潔にします。',
         image: 'https://static.vecteezy.com/vite/assets/photo-masthead-375-BoK_p8LG.webp',
+        link: '',
     },
 ];
 
