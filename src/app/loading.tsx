3e5f2cdd4e88a2618b448c48d91492e806a0d920
satty-dev <sadahiro0814@gmail.com>
2025-04-26 13:40:16 +0900

    【Loading】リッチ化

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,23 @@
+// MUI
+import { CircularProgress, Box, Typography } from '@mui/material';
+
 export default function Loading() {
     return (
-        <div className='mt-10 flex items-center justify-center gap-6'>
-            <div className='h-10 w-10 animate-spin rounded-full border-[5px] border-sky-400 border-t-transparent'></div>
-            <p className='font-weight text-[30px]'>Loading</p>
-        </div>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '100vh',
+                gap: 2,
+            }}>
+            <CircularProgress />
+            <Typography
+                variant='subtitle1'
+                color='text.secondary'>
+                Loading my awesome-ness...
+            </Typography>
+        </Box>
     );
 }
