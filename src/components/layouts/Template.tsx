d3b8ee3431d0c4abaec4ee0d2a4940b28f578deb
satty-dev <sadahiro0814@gmail.com>
2025-04-29 13:46:53 +0900

    【Template】全体レイアウトの調整とヘッダーの固定

--- src/components/layouts/Template.tsx
+++ src/components/layouts/Template.tsx
@@ -1,9 +1,9 @@
 // MUI
-import Container from '@mui/material/Container';
+import { Box, Container } from '@mui/material';
 
 // components
 import { Header } from '@/components/layouts/Header';
-import { Footer } from '@/components/layouts/Footer'; 
+import { Footer } from '@/components/layouts/Footer';
 
 type Template = {
     children: React.ReactNode;
@@ -11,14 +11,35 @@ type Template = {
 
 export const Template = (props: Template) => {
     const { children } = props;
+    const headerHeight: number = 64;
 
     return (
-        <div>
-            <Header />
-            <Container maxWidth='lg'>
-                <main>{children}</main>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                minHeight: '100vh',
+            }}>
+            <Box sx={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                right: 0,
+                zIndex: 1100,
+            }}>
+                <Header />
+            </Box>
+            <Container
+                component='main'
+                maxWidth='lg'
+                sx={{
+                    flexGrow: 1,
+                    mt: `${headerHeight}px`,
+                    py: 4,
+                }}>
+                {children}
             </Container>
             <Footer />
-        </div>
+        </Box>
     );
 };
