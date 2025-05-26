16fcc4732204b887c74457b31ee5d3d60b4b2c91
satty-dev <sadahiro0814@gmail.com>
2025-05-26 17:47:42 +0900

    【Works】カードのテキストリンクをカード全体へ

--- src/components/materials/MediaCaed.tsx
+++ src/components/materials/MediaCaed.tsx
@@ -3,10 +3,9 @@ import Link from 'next/link';
 
 // MUI
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 // types
@@ -19,34 +18,40 @@ type MediaCardProps = {
 export const MediaCard = (props: MediaCardProps) => {
     const { work } = props;
     return (
-        <Card sx={{ maxWidth: 'xs' }}>
-            <CardMedia
-                sx={{ height: 140 }}
-                image={work.image}
-                title={work.title}
-            />
-            <CardContent>
-                <Typography
-                    gutterBottom
-                    variant='h5'
-                    component='div'>
-                    {work.title}
-                </Typography>
-                <Typography
-                    variant='body2'
-                    sx={{ color: 'text.secondary' }}>
-                    {work.description}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size='small'>Share</Button>
-                <Button
-                    component={Link}
-                    href={`/works/${work.id}`}
-                    size='small'>
-                    Learn More
-                </Button>
-            </CardActions>
+        <Card
+            sx={{
+                maxWidth: 'xs',
+                transition: 'opacity 0.3s',
+                opacity: 1,
+                '&:hover': {
+                    opacity: 0.8,
+                },
+            }}>
+            <Link
+                href={`/works/${work.id}`}
+                passHref
+                legacyBehavior>
+                <CardActionArea component='a'>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image={work.image}
+                        title={work.title}
+                    />
+                    <CardContent>
+                        <Typography
+                            gutterBottom
+                            variant='h5'
+                            component='div'>
+                            {work.title}
+                        </Typography>
+                        <Typography
+                            variant='body2'
+                            sx={{ color: 'text.secondary' }}>
+                            {work.description}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Link>
         </Card>
     );
 };
